0f3cfca48fee4b328b0d19758aed1c24a852767d
naoki <manzisann@gmail.com>
2017-10-02 03:06:45 +0900

    Update randome text js

--- index.php
+++ index.php
@@ -5,282 +5,34 @@
 <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
 <link rel="stylesheet" type="text/css" href="./build/css/style.css">
 <script src="https://code.jquery.com/jquery-3.2.0.js"></script>
-<script type="text/javascript" src="js/script.js"></script>
+<!-- <script type="text/javascript" src="js/script.js"></script> -->
   </head>
     <title>KUMAKURA NAOKI.com</title>
   </head>
+  <style>
+    #inName {
+      transition: 0.5s;
+      font-size: 500px;
+      font-weight: bold;
+    }
+  </style>
 <body>
-  <main>
-
-    <!-- home -->
-    <div class="content-box">
-      <div class="colomn-content">
-        <!-- <div class="colomn-img">
-          <img src="img/home.png">
-          <div>home</div>
-        </div> -->
-      <h1>HOME</h1>
-      <div class="colomn-main">
-        <div id="home">
-
-        <div id="inName"></div>
-
-        <table class="update">
-          <tr>
-            <td>2017,06,18</td>
-            <td>change this project framework.</td>
-          </tr>
-          <tr>
-            <td>2016,12,15</td>
-            <td>adjustment mail form.</td>
-          </tr>
-          <tr>
-            <td>2016,11,23</td>
-            <td>change top page animation.</td>
-          </tr>
-          <tr>
-            <td>2016,11,11</td>
-            <td>Update my skill List.</td>
-          </tr>
-        </table>
-        </div><!--home-->
-      </div>      
-      </div><!-- colomn-content -->
-    </div><!-- content-box -->
     
-    <!-- my infomation -->
-    <div class="content-box">
-      <div class="colomn-content">
-        <!-- <div class="colomn-img">
-        <div>profile</div>
-        </div> -->
-        <h1>PROFILE</h1>
-        <div class="colomn-main">
-          <div id="profile">
-            <div class="icon-box">
-              <div class="icon">
-                <a href="https://github.com/manzisan"><img src="img/git.png" alt=""></a>
-              </div>
-              <div class="icon">
-                <a href=""><img src="img/facebook.png" alt=""></a>
-              </div>
-              <div class="icon">
-                <a href=""><img src="img/twitter.png" alt=""></a>
-              </div>
-              <div class="icon">
-                <a href="http://steamcommunity.com/id/manzisan"><img src="img/steam.png" alt="steam"></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div><!-- colomn-content -->    
-    </div><!-- content-box -->
+  <div id="inName"></div>
 
-    <!-- skills -->
-    <div class="content-box">
-      <div class="colomn-content">
-        <!-- <div class="colomn-img">
-          <div>skill</div>
-        </div> -->
-      <h1>SKILL</h1>
-      <div class="colomn-main">
-        <div id="skill">
-          <p class="skill-title">WEB</p>
-          
-          <div class="chartbox">
-            <p>HTML</p>
-            <svg class="backsvg"></svg>
-            <svg class="chartsvg"></svg>
-            <div class="percent"></div>
-          </div>
-
-          <div class="chartbox">
-            <p>CSS</p>
-            <svg class="backsvg"></svg>
-            <svg class="chartsvg"></svg>
-            <div class="percent"></div>
-          </div>
-
-          <div class="chartbox">
-            <p>JAVA SCRIPT</p>
-            <svg class="backsvg"></svg>
-            <svg class="chartsvg"></svg>
-            <div class="percent"></div>
-          </div>
-
-          <div class="chartbox">
-            <p>PHP</p>
-            <svg class="backsvg"></svg>
-            <svg class="chartsvg"></svg>
-            <div class="percent"></div>
-          </div>
-
-          <div class="chartbox">
-            <p>RUBY</p>
-            <svg class="backsvg"></svg>
-            <svg class="chartsvg"></svg>
-            <div class="percent"></div>
-          </div>
-
-          <div class="skill-detail">
-
-            <div class="skill-detail">
-              <p class="skill-title">html detail</p>
-
-              <div class="chartbox">
-                <p>HTML 5</p>
-                <svg class="backsvg"></svg>
-                <svg class="chartsvg"></svg>
-                <div class="percent"></div>
-              </div>
-              <div class="chartbox">
-                <p>XML</p>
-                <svg class="backsvg"></svg>
-                <svg class="chartsvg"></svg>
-                <div class="percent"></div>
-              </div>
-              <div class="chartbox">
-                <p>XSL</p>
-                <svg class="backsvg"></svg>
-                <svg class="chartsvg"></svg>
-                <div class="percent"></div>
-              </div>
-            </div>
-
-            <div class="skill-detail">
-              <p class="skill-title">css detail</p>
-              <p>1</p>
-              <p>2</p>
-              <p>3</p>
-              <p>4</p>
-              <p>5</p>
-            </div>
-
-            <div class="skill-detail">
-              <h2>js-detail</h2>
-              <p>1</p>
-              <p>2</p>
-              <p>3</p>
-              <p>4</p>
-              <p>5</p>
-            </div>
-
-            <div class="skill-detail">
-              <h2>php-detail</h2>
-              <p>1</p>
-              <p>2</p>
-              <p>3</p>
-              <p>4</p>
-              <p>5</p>
-            </div>
-
-            <div class="skill-detail">
-              <h2>ruby-detail</h2>
-              <p>1</p>
-              <p>2</p>
-              <p>3</p>
-              <p>4</p>
-              <p>5</p>
-            </div>
-
-          </div>
-        </div><!-- skill -->
-        
-      </div>     
-      </div><!-- colomn-content -->
-    </div><!-- content-box -->
-    
-    <!-- works -->
-    <div class="content-box">
-      <div class="colomn-content">
-        <!-- <div class="colomn-img">
-          <img src="img/work.png">
-          <div>works</div>
-        </div> -->
-          <h1>WORKS</h1>
-          <div class="colomn-main">
-            <div id="works">
-              <div class="work-design">
-                  
-
-
-
-
-
-
-              </div>
-            </div><!--works -->
-          </div><!-- colomn-main -->
-      </div><!-- colomn-content -->
-    </div><!-- content-box -->
-
-    <!-- contact -->
-    <div class="content-box">
-      <div class="colomn-content">
-        <!-- <div class="colomn-img">
-        <img src="img/mail.png">
-        <div>CONTACT</div>
-        </div> -->
-        <h1>CONTACT</h1>
-        <div class="colomn-main">
-          <div id="contact">
-            <form action="sendMessage.php" method="post">
-              <div class="input-column">
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name">
-              </div>
-              <div class="input-column">
-                <label for="message">Message</label>
-                <textarea name="message" id="message"></textarea>
-              </div>
-              <input type="submit" value="send" id="sendmail">
-            </form> 
-            <div class="error">
-              <p class="hidden-text">plz enter your name</p>
-              <p class="hidden-text">plz enter message</p>
-            </div>
-            <p class="hidden-text">Thanx.</p>
-          </div><!-- contact -->
-        </div><!-- colomn-main -->
-      </div><!-- colomn-content -->
-    </div><!-- content-box -->
-
-  </main>
 </body>
+<script type="text/javascript" src="js/randomtext.js"></script>
 <script>
-
-  var main = document.getElementsByTagName('main');
- 
-  //スクロール禁止用関数
-  function no_scroll(){
-    //PC用
-    var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-
-    $(document).on(scroll_event,function(e){
-      e.preventDefault();
-    });
-    //SP用
-    $(document).on('touchmove.noScroll', function(e) {
-      e.preventDefault();
-    });
+  var pxNum = 500;
+  var pxCount = 5;
+  function changePx() {
+    pxCount+=1;
+    pxNum-=pxCount;
+    $('#inName').css({'font-size':pxNum+'px'});
   }
-   
-  //スクロール復活用関数
-  function return_scroll(){
-    //PC用
-    var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-    $(document).off(scroll_event);
-    //SP用
-    $(document).off('.noScroll');
+  setInterval(function(){
+    changePx();
+  },100);
 
-  }
-
-  no_scroll();
-  
-  console.log(window.parent.screen.width);
-
-  if (window.parent.screen.width == "375") {
-    return_scroll();
-  }
 </script>
 </html>
--- js/randomtext.js
+++ js/randomtext.js
@@ -1,69 +1,47 @@
-// random text
-var abc = new Array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-var myName = new Array("K","U","M","A","K","U","R","A","N","A","O","K","I",".","c","o","m");
+window.onload = function() {
 
-var flag = true;
-var num = 0;
-var inName = document.getElementById('inName');
-var nameBox = document.getElementsByClassName('namebox');
-var length = myName.length;
-var interval = setInterval(randomChange,50);
+  var abc = new Array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+  var myName = new Array("K","U","M","A","K","U","R","A","N","A","O","K","I",".","c","o","m");
 
-  for (var i = 0; i < myName.length; i++){
+  var flag = true;
+  var num = 0;
+  var inName = document.getElementById('inName');
+  var nameBox = document.getElementsByClassName('namebox');
+
+  for (var i = 0; i < myName.length; i++) {
     inName.innerHTML += "<span class='namebox'></span>";
   }
 
-  function start(){
-    flag = false;
-    setName();
-  }
-
-  function randomChange(){
+  function randomChange() {
     for (var i = 0; i < myName.length; i++) {
       var random = Math.floor( Math.random() * 22 );
-      if(flag === true){
+      if(flag) {
         nameBox[i].innerHTML = abc[random];
       }
-      if(flag === false){
-        try{
-            nameBox[num+i].innerHTML = abc[random];
-        }catch(e){
-          // no alert log
+      if(!flag) {
+        try {
+          nameBox[num + i].innerHTML = abc[random];
+        } catch(e) {
+          // for no alert
         }
       }
     }
   }
 
-var h1 = document.getElementsByTagName('h1');
-var update = document.getElementsByClassName('update');
-var slidenum = 0;
-
-  function setName(){
-  var random = Math.floor( Math.random() * 18 );
+  function setName() {
+    var random = Math.floor( Math.random() * myName.length );
     nameBox[num].innerHTML = myName[num];
-      num++;
-        if (num == length){
-        var slide = setInterval(function(){
-          if (slidenum > 1){
-            c_box[slidenum-1].style.width="10%";
-          }
-          if (c_box.length == slidenum) {
-            clearInterval(slide);
-          }
-          c_box[slidenum].style.width="60%";
-          c_box[slidenum].style.display="block";
-          c_box[slidenum].style.opacity="1";
-          slidenum++;
-        },150);// setInterval
-        flag = "";
-        setWindow();
-        clearInterval(interval);
-        for (var i = 0; i < h1.length; i++) {
-          h1[i].style.opacity="1";
-        }
-        update[0].style.opacity="1";
-        return;
-        }//length
+    num++;
+    // if end of random change logic
+    if (num == myName.length) {
+      flag = "";
+      return;
+    }
     setTimeout(setName,100);
   }
-  start();
+
+  flag = false;
+  setInterval(randomChange,50);
+  setName();
+
+}
