84f4b09200422b1b984f7726951fbdf721a5a3d1
naoki <manzisann@gmail.com>
2018-03-08 19:21:20 +0900

    Update animation

--- index.php
+++ index.php
@@ -9,52 +9,61 @@
     <title>KUMAKURA NAOKI.com</title>
   </head>
 <body>
-  <header>
-    <h1>Kumakura Naoki.com</h1>
-  </header>
-  <div class="close">Back</div>
-  <div class="container clear">
-    <?php
-      $title = [
-        "It's Me",
-        "Works",
-        "Skill",
-        "Sharing knowledge",
-        "Affiliation",
-        "Contact",
-        "Codepen",
-        "Steam",
-        "Play Station.net",
-        "Discord",
-        "fashion",
-      ];
-      for ($i=0; $i < 11; $i++) :
-    ?>
-      <div class="box">
-        <div class="text">
-          <?= $title[$i] ?>
-        </div>
+  <div id="Wrap">
+    <header>
+      <h1>Kumakura Naoki.com</h1>
+      <div>
+        
       </div>
-    <?php endfor; ?>
+    </header>
+    <div class="close">Close</div>
+    <div class="container clear">
+      <?php
+        $title = [
+          "It's Me",
+          "Works",
+          "Skill",
+          "Sharing knowledge",
+          "Affiliation",
+          "Contact",
+          "Codepen",
+          "Steam",
+          "Play Station.net",
+          "Discord",
+          "fashion",
+        ];
+        for ($i=0; $i < 11; $i++) :
+      ?>
+        <div class="box">
+          <div class="text">
+            <?= $title[$i] ?>
+          </div>
+        </div>
+      <?php endfor; ?>
+    </div>
+    <footer>
+      <small>Copyright 2018 naoki kumakura as manzisan</small>
+    </footer>
   </div>
-  <footer>
-    <small>Copyright 2018 naoki kumakura as manzisan</small>
-  </footer>
 </body>
 <script>
   $(".box").on("click",function(){
     $(".close").show();
-    $(this).css({
-      "width":"100%",
-      "height": "500px",
-      "transform":"rotate(0deg)",
-      "background-color": "#fff",
-      "color": "#000"
-    }),
+    $(this).addClass("view-mode");
+    $(".container").addClass("view-mode-container");
+    $(".box").not($(this)).fadeOut();
     $(".text").css({
       "transform":"rotate(0deg)",
     })
-    $(".box").not($(this)).hide();
+  });
+  $(".close").on("click",function(){
+    $(".close").hide();
+    $(".box").removeClass("view-mode");
+    $(".container").removeClass("view-mode-container");
+    $(".box").not($(this)).fadeIn();
+    $(".text").css({
+      "transform":"rotate(-45deg)",
+    })
   });
 </script>
 </html>
