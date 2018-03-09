dbd6b9b903a28b38961432e4de87f43ea854bf43
naoki <manzisann@gmail.com>
2018-03-09 12:41:35 +0900

    Update animation and common layout

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <meta charset="utf-8">
 <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
 <link rel="stylesheet" type="text/css" href="./build/css/style.css">
+<!-- <link rel="stylesheet" href="http://manz1.jellybean.jp/css/style.css"> -->
+<link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.2.0.js"></script>
   </head>
     <title>KUMAKURA NAOKI.com</title>
@@ -12,25 +14,27 @@
   <div id="Wrap">
     <header>
       <h1>Kumakura Naoki.com</h1>
-      <div>
+      <div class="icon">
         
       </div>
     </header>
-    <div class="close">Close</div>
+    <div class="close">
+      <span>Close</span>
+    </div>
     <div class="container clear">
       <?php
         $title = [
-          "It's Me",
-          "Works",
-          "Skill",
-          "Sharing knowledge",
-          "Affiliation",
-          "Contact",
-          "Codepen",
-          "Steam",
-          "Play Station.net",
-          "Discord",
-          "fashion",
+          "It's Me <i class='fa fa-user'></i>",
+          "Works <i class='fa fa-code'></i>",
+          "Skill <i class='fa fa-chart-pie'></i>",
+          "Github <i class='fab fa-github'></i>",
+          "Sharing knowledge <i class='fa fa-share-alt'></i>",
+          "Contact <i class='fa fa-envelope'></i>",
+          "Codepen <i class='fab fa-codepen'></i>",
+          "Company <i class='fa fa-building'></i>",
+          "Steam <i class='fab fa-steam'></i>",
+          "Play Station.net <i class='fab fa-playstation'></i>",
+          "Discord <i class='fab fa-discord'></i>",
         ];
         for ($i=0; $i < 11; $i++) :
       ?>
@@ -51,16 +55,16 @@
     $(".close").show();
     $(this).addClass("view-mode");
     $(".container").addClass("view-mode-container");
-    $(".box").not($(this)).fadeOut();
+    $(".box").not($(this)).hide();
     $(".text").css({
       "transform":"rotate(0deg)",
     })
   });
-  $(".close").on("click",function(){
+  $(".close span").on("click",function(){
     $(".close").hide();
     $(".box").removeClass("view-mode");
     $(".container").removeClass("view-mode-container");
-    $(".box").not($(this)).fadeIn();
+    $(".box").not($(this)).show();
     $(".text").css({
       "transform":"rotate(-45deg)",
     })
